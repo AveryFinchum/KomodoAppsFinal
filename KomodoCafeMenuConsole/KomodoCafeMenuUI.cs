97972611e40907bfa21191298f5a6d170945285f
avery finchum <avfinchum@gmail.com>
2022-03-23 10:50:38 -0400

    Update Menu UI name, Add Claims project

--- KomodoCafeMenuConsole/KomodoCafeMenuUI.cs
+++ KomodoCafeMenuConsole/KomodoCafeMenuUI.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace KomodoCafeMenuConsole
 {
-    internal class KomodoCafeMenu
+    internal class KomodoCafeMenuUI
     {
     }
 }
---
+++ UnitTestProject1/KomodoClaimsUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class KomodoClaimsUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
