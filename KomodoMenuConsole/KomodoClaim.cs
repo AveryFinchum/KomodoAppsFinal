eda8277be79d0cba0e0dd0bc2b918420f32a537b
avery finchum <avfinchum@gmail.com>
2022-03-24 12:12:05 -0400

    Created Simple claims object

--- KomodoMenuConsole/KomodoClaim.cs
+++ KomodoMenuConsole/KomodoClaim.cs
@@ -19,24 +19,46 @@ namespace _KomodoClaims
         ///     DateOfClaim
         ///     IsValid: Only valid if DateOfCaim - DateOfIncident <= 30 days
         /// </summary>
-        public enum ClaimType { Car = 1, Home, Theft }
+        public enum ClaimTypes { Car = 1, Home, Theft }
 
         public KomodoClaim() { }
-        public KomodoClaim(int id, ClaimType claimType, string description, decimal claimAmount, string ingredients)
+        public KomodoClaim(int id, ClaimTypes claimType, string description, decimal claimAmount, DateTime incidentDate, DateTime dateClaimFiled, bool isValid)
         {
             ClaimID = id;
             ClaimType = claimType;
             Description = description;
             ClaimAmount = claimAmount;
-            Ingredients = ingredients;
+            DateOfIncident = incidentDate;
+            DateOfClaim = dateClaimFiled;
+
+            TimeSpan claimTimeSpan = new TimeSpan(0); // creeate empty timespan
+            if(claimTimeSpan > TimeSpan.FromDays(0))
+
+            try
+            {
+                claimTimeSpan = dateClaimFiled - incidentDate; // Checking to see if the user input 
+            }
+            finally { }
+
+
+            if (claimTimeSpan <= TimeSpan.FromDays(30) && claimTimeSpan > TimeSpan.FromDays(0)) // If timespan was valid and if it is less than 30 days
+            {
+                IsValid = true; // then the claim is valid
+            }
+            else
+            {
+                IsValid = false;// otherwise the claim will not be valid
+            }
         }
 
         //Required fields
         public int ClaimID { get; set; }
-        public ClaimType ClaimType { get; set; }
+        public ClaimTypes ClaimType { get; set; }
         public string Description { get; set; }
-        public string Ingredients { get; set; }
         public decimal ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        private bool IsValid { get; set; }
 
     }
 }
--- KomodoMenuConsole/KomodoClaimsConsoleUI.cs
+++ KomodoMenuConsole/KomodoClaimsConsoleUI.cs
@@ -69,6 +69,7 @@ namespace _KomodoClaimsMenuConsole
         }
 
 
+
         //************************************HELPER METHODS*************************************
         private void DisplayContent(KomodoClaim item)
         {
@@ -76,7 +77,7 @@ namespace _KomodoClaimsMenuConsole
                $"Meal Number: {item.ClaimID}\n" +
                $"Name:        {item.ClaimType}\n" +
                $"Description: {item.Description}\n" +
-               $"Ingredients: {item.Ingredients}\n" +
+               $"Ingredients: {item.DateOfIncident}\n" +
                $"Price:       {item.ClaimAmount}\n");
         }
         private void ConsoleWelcomeScreen()
