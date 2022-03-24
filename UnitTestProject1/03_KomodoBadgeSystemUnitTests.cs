e87eaf1eedfba3c2dec279d0e320760baf18e471
avery finchum <avfinchum@gmail.com>
2022-03-24 16:52:46 -0400

    Unit tests scaffoled out for claims and unit tests

--- UnitTestProject1/03_KomodoBadgeSystemUnitTests.cs
+++ UnitTestProject1/03_KomodoBadgeSystemUnitTests.cs
@@ -7,8 +7,72 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddNumber_ShouldGetCorrectNumberBack()
         {
+            //Arrange
+            MenuItem meal = new MenuItem();
+
+            meal.Number = 4;
+
+            //Act
+            int expected = 4;
+            int actual = meal.Number;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void AddToRepo_ShouldGetTrueBack()
+        {
+            // Arrange
+            MenuItem item = new MenuItem();
+            KomodoCafeMenuRepo repo = new KomodoCafeMenuRepo();
+            // Act
+            bool addResult = repo.AddItemToMenuList(item);
+            // Assert
+            Assert.IsTrue(addResult);
+        }
+
+        [TestMethod]
+        public void GetDirecty_ShouldReturnCorrectCollection()
+        {
+            // Arrange
+            MenuItem item = new MenuItem();
+            KomodoCafeMenuRepo repo = new KomodoCafeMenuRepo();
+
+            repo.AddItemToMenuList(item);
+            // Act
+            List<MenuItem> MenuItems = repo.GetMenuItems();
+
+            bool directoryHasContent = MenuItems.Contains(item);
+
+            // Assert
+            Assert.IsTrue(directoryHasContent);
+        }
+
+        private MenuItem _content;
+        private KomodoCafeMenuRepo _repo;
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new KomodoCafeMenuRepo();
+            _content = new MenuItem(1, "Human Food Name", "Some normal Human food. Mmm, tasty...", "Normal Vegtable, Normal Fruit, Some normal protien, mmmm...", 4.20m);
+            _repo.AddItemToMenuList(_content);
+        }
+
+        [TestMethod]
+        public void DeleteExistingMenuItem_ShouldReturnTrue()
+        {
+            // Arrange
+            Arrange();
+
+            // Act
+            bool removeResult = _repo.DeleteExistingMenuItem(_content);
+
+            // Assert
+            Assert.IsTrue(removeResult);
+        }
+    }
     }
 }
